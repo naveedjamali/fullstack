c9a02147216a222f62cede3142cda0bddad37da1
Naveed Jamali <naveedjamali@outlook.com>
2022-12-21 22:54:02 +0500

    template string

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+let fname = 'Ryan';
+let lname = 'D';
+let age = prompt("Guess Ryan's age");
+
+// //OLD way
+
+// const result = fname +' '+lname+' '+age+' years old';
+// alert(result);
 
 
-if (true) {
-    let name = "Ryan";
-}
-
-
-alert(name);
+let result = `${fname} ${lname} is ${age} years old`;
+alert(result);
