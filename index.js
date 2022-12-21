ccc582bcc83f81f5faf287be1ae47dc1c5874d1c
Naveed Jamali <naveedjamali@outlook.com>
2022-12-21 23:12:21 +0500

    arrow functions

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-function welcome(user="Mystery Person", message='Good day to you'){
-    alert(`Hello ${user}, ${message}`);
+// function greatings(message){
+//     alert(`${message} everyone!`)
+// }
+
+// let greatings = (message)=>alert(`${message} everyone!`);
+
+// greatings('Good morning'); 
+
+let createBlog = (title,body)=>{
+
+
+    if(!title){
+        throw new Error('A title is required')
+    }
+
+    if(!body){
+        throw new Error('A body is required')
+
+
+    }
+
+    return alert(`${title} - ${body}`);
 }
 
-welcome('Jakson');
+createBlog('Confirm','Confirm that you are an alien'); 
