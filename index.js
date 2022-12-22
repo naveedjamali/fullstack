ed5e68e505936d5943bd725056020a5358e5fb67
Naveed Jamali <naveedjamali@outlook.com>
2022-12-22 11:21:26 +0500

    desctructering

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-let thingsTodo = {
-    morning:"Exercise",
-    afternoon:'coding',
-    evening:'Watch movies',
-    night:['Sleep','Dream']
-}
+let uniStudent = student => { 
+    let {name, university} = student;
+    console.log(`${name} from ${university}`);
 
-let {morning,afternoon} = thingsTodo;
+};
 
-console.log(morning);
-console.log(afternoon);
-thingsTodo.morning='Singing';
-console.log('morning in windows',morning);
-console.log('morning in object',thingsTodo.morning);
+uniStudent({ name: 'Ryan', university: 'Harvard University' });
