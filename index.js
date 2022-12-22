5658dc3eae853e5d2ec733de0f880d71e6758d40
Naveed Jamali <naveedjamali@outlook.com>
2022-12-22 10:33:53 +0500

    removed old code

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
-// function greatings(message){
-//     alert(`${message} everyone!`)
-// }
-
-// let greatings = (message)=>alert(`${message} everyone!`);
-
-// greatings('Good morning'); 
-
-let createBlog = (title,body)=>{
+function sayHi(){
+    alert('Hi');
 
 
-    if(!title){
-        throw new Error('A title is required')
-    }
-
-    if(!body){
-        throw new Error('A body is required')
-
-
-    }
-
-    return alert(`${title} - ${body}`);
 }
 
-createBlog('Confirm','Confirm that you are an alien'); 
+console.log(this )
