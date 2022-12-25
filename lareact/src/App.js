39ffe3e40a12b1c7af2480952b6b92fab521f1bc
Naveed Jamali <naveedjamali@outlook.com>
2022-12-25 22:48:00 +0500

    react

--- lareact/src/App.js
+++ lareact/src/App.js
@@ -1,30 +1,21 @@
-import axios from "axios";
-import { Component } from "react";
-
+const { Component } = require("react");
 
 class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {users:[]}
+    this.state = {
+      users: [],
+    }
   }
-
-
   componentWillMount(){
     
-    axios.get('https://api.randomuser.me/?nat=US&results=5').then(function(response){
-      console.log(response.data.results);
-
-        });
   }
-
   render() {
-    return <>
-      <div className='App'>
-        We will be back
-      </div>
-    </>
-  };
+    return <div>
+      We will be back;
+    </div>
+  }
 }
 
 export default App;
